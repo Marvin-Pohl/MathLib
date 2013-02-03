08183348eea6fe4f625ffe8bf19b9598ad9261a1
Marvin Pohl <marvins_adresse@gmx.de>
2013-02-03 01:22:20 +0100

    Changed pch to use #defines.

--- MathLib/MathLib/include/precompiledHeader.h
+++ MathLib/MathLib/include/precompiledHeader.h
@@ -2,8 +2,13 @@
 Precompiled header for the MathLib
 
 */
+#define DLLImport	__declspec( dllimport )
+#define DLLExport	__declspec( dllexport )
 
-#define DLLImport __declspec( dllimport )
-#define DLLExport __declspec( dllexport )
+//#define _DOUBLE_PRECISION
 
-#define Scalar float
+#ifdef _DOUBLE_PRECISION
+typedef double Scalar;
+#else
+typedef float Scalar;
+#endif
