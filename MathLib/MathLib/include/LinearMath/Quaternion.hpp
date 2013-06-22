8ac9568409be2216969ec831a8285875a5a73703
Marvin Pohl <marvins_adresse@gmx.de>
2013-06-22 12:02:11 +0200

    Added static method for axis-angle representation for quaternion.

--- MathLib/MathLib/include/LinearMath/Quaternion.hpp
+++ MathLib/MathLib/include/LinearMath/Quaternion.hpp
@@ -3,9 +3,9 @@
 	#define Quaternion_h__
 
 	#include "MathLibPCH.hpp"
-#include "MathHelper.hpp"
-#include "Vector3.hpp"
-#include "Matrix4x4.hpp"
+	#include "MathHelper.hpp"
+	#include "Vector3.hpp"
+	#include "Matrix4x4.hpp"
 
 namespace LinearMath
 {
@@ -16,7 +16,7 @@ namespace LinearMath
 
 		Quaternion_tpl( void );
 
-		Quaternion_tpl( const ScalarType x, const ScalarType y, const ScalarType z, const ScalarType w );
+		Quaternion_tpl( const ScalarType w, const ScalarType x, const ScalarType y, const ScalarType z );
 
 		Quaternion_tpl( const Vector3_tpl< ScalarType >& axis, const Radian_tpl< ScalarType >& angle );
 
@@ -24,22 +24,33 @@ namespace LinearMath
 
 		Matrix4x4_tpl< ScalarType > ToRotationMatrix( void ) const;
 
+		static Quaternion_tpl< ScalarType > FromAngleAxis(
+			Vector3_tpl< ScalarType >& axis, const Radian_tpl< ScalarType >& angle );
+
 		Quaternion_tpl< ScalarType > operator *( const Quaternion_tpl< ScalarType >& quat ) const;
 
 		Radian_tpl< ScalarType > GetAngle( void ) const;
 
-		ScalarType X, Y, Z, W;
+		ScalarType W, X, Y, Z;
 
 	private:
 	};
 
 	template< typename ScalarType >
 	LinearMath::Quaternion_tpl< ScalarType >::Quaternion_tpl( void ) :
+		W( 1 ),
 		X( 0 ),
 		Y( 0 ),
-		Z( 0 ),
-		W( 1 )
+		Z( 0 )
 	{
+	}
+
+	template< typename ScalarType >
+	Quaternion_tpl< ScalarType > LinearMath::Quaternion_tpl< ScalarType >::FromAngleAxis(
+		Vector3_tpl< ScalarType >& axis,
+		const Radian_tpl< ScalarType >& angle )
+	{
+		return Quaternion_tpl< ScalarType >( axis, angle );
 	}
 
 	template< typename ScalarType >
@@ -52,21 +63,21 @@ namespace LinearMath
 		W = MathHelper_tpl< ScalarType >::Cos( halfAngle );
 
 		halfAngle = MathHelper_tpl< ScalarType >::Sin( halfAngle );
-		X = halfAngle axis.X;
-		Y = halfAngle axis.Y;
-		Z = halfAngle axis.Z;
+		X = halfAngle * axis.X;
+		Y = halfAngle * axis.Y;
+		Z = halfAngle * axis.Z;
 	}
 
 	template< typename ScalarType >
 	LinearMath::Quaternion_tpl< ScalarType >::Quaternion_tpl(
+		const ScalarType w,
 		const ScalarType x,
 		const ScalarType y,
-		const ScalarType z,
-		const ScalarType w ) :
+		const ScalarType z ) :
+		W( w ),
 		X( x ),
 		Y( y ),
-		Z( z ),
-		W( w )
+		Z( z )
 	{
 	}
 
@@ -97,12 +108,12 @@ namespace LinearMath
 		ScalarType zw = 2 * Z * W;
 
 		return Matrix4x4_tpl< ScalarType >
-			(
+			   (
 			ww + xx - yy - zz, xy + zw, xz - yw, 0,
 			xy - zw, ww - xx + yy - zz, yz + xw, 0,
 			xz + yw, yz - xw, ww - xx - yy + zz, 0,
 			0, 0, 0, 1
-			);
+			   );
 	}
 
 	template< typename ScalarType >
