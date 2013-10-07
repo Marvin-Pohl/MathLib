f363299e911e6ff1d13cc6ade3437b5f3cae61e2
Marvin Pohl <mp078@hdm-stuttgart.de>
2013-10-07 10:32:53 +0200

    Fixed a Spelling Error in PCH.

--- MathLib/MathLib/include/MathLibPCH.hpp
+++ MathLib/MathLib/include/MathLibPCH.hpp
@@ -4,13 +4,13 @@
 Precompiled header for the MathLib
 
 */
-#define MATHLIB_DLL_IIMPORT	__declspec( dllimport )
+#define MATHLIB_DLL_IMPORT	__declspec( dllimport )
 #define MATHLIB_DLL_EXPORT	__declspec( dllexport )
 
 #ifdef BUILD_MATHLIB_API
 #define MATHLIB_API MATHLIB_DLL_EXPORT
 #else
-#define MATHLIB_API MATHLIB_DLL_IIMPORT
+#define MATHLIB_API MATHLIB_DLL_IMPORT
 #endif // BUILD_MATHLIB_API
 
 namespace Numerics
